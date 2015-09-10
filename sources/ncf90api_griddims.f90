dee468617491823f727753dfd9401cfb1eaa0f61
Fernando (at work) <fernando.m.pimenta@gmail.com>
2015-09-10 13:38:17 -0300

    Add call to checkfile error subroutine

--- sources/ncf90api_griddims.f90
+++ sources/ncf90api_griddims.f90
@@ -138,8 +138,9 @@ subroutine ncoords2d_double(ifile, idata)
   character(*), intent(in) :: ifile
 
   !Open NetCDF File
-  call check(nf90_open(ifile, nf90_nowrite, ncid))
-
+  write(*,*)"AAAAAAAAAAA"
+  call checkfile(nf90_open(ifile, nf90_nowrite, ncid))
+  write(*,*)"GGGGGGGGGGG"
   !Get Coordinate values and put them in nx, ny
   call check(nf90_inq_dimid(ncid, idata%lonname, xdimid))
   call check(nf90_inquire_dimension(ncid, xdimid, idata%lonname, idata%nlons))
