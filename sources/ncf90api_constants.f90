11e7ee42ee4690077d986955fa35234dde3ba0ef
Fernando (at work) <fernando.m.pimenta@gmail.com>
2015-09-18 12:15:13 -0300

    Add type parameters: byte, short, intgr, float, double

--- sources/ncf90api_constants.f90
+++ sources/ncf90api_constants.f90
@@ -32,7 +32,13 @@
 !:========================================================================
 
 !:========================== Constants ===================================
-real*8, parameter :: pi = 4*atan(1.0)
-real*8, parameter :: earth_radius = 6371.0  ! average radius of earth ~ km
-real*8, parameter :: acc_gravity = 9.80616     ! acceleration of gravity ~m/s^2
-real*8, parameter :: boltzman= 5.67e-8    ! Stefan-Boltzmann constant ~W/m^2/K^4
+integer, parameter :: byte = C_INT
+integer, parameter :: short = C_SHORT
+integer, parameter :: intgr = C_INT
+integer, parameter :: float = C_FLOAT
+integer, parameter :: double = C_DOUBLE
+
+real(kind=double), parameter :: pi = 4*atan(1.0)
+real(kind=double), parameter :: earth_radius = 6371.0  ! average radius of earth ~ km
+real(kind=double), parameter :: acc_gravity = 9.80616     ! acceleration of gravity ~m/s^2
+real(kind=double), parameter :: boltzman= 5.67e-8    ! Stefan-Boltzmann constant ~W/m^2/K^4
