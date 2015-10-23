6828c8e1936013fe26d9f0f5b53802fe30c5d233
Fernando (at work) <fernando.m.pimenta@gmail.com>
2015-10-23 18:25:41 -0200

    Added corrections in zonal_<type> for each subroutine type

---
+++ shell_gencodes/ncf90api_zonalstats.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+echo "!:========================================================================
+! This file is part of f90NetCDF API (Fortran 90 API for NetCDF).
+
+! Copyright (C) 2015 Fernando Martins Pimenta
+
+! f90NetCDF API is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+
+! f90NetCDF API is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+! You should have received a copy of the GNU General Public License
+! along with f90NetCDF.  If not, see <http://www.gnu.org/licenses/>.
+
+!:========================================================================
+!About Author:
+!Student of Surveying and Cartographic Engineering
+! Federal University of Viçosa - Brazil
+
+!Bachelor of Biosystems Engineer
+! Federal University of São João del-Rei - Brazil
+
+! Research Group on Atmosphere-Biosphere Interaction
+! Federal University of Viçosa
+! Data: August 09, 2015
+
+!Contacts: fernando.m.pimenta@gmail.com, fernando.m.pimenta@ufv.br
+!:========================================================================"
+
+declare -a arr=("byte" "short" "int" "float" "double")
+declare -a arrid=("b" "s" "i" "f" "d")
+declare -a arr2=("integer(kind=byte)" "integer(kind=short)" "integer(kind=intgr)" "real(kind=float)" "real(kind=double)")
+
+echo "!Zonal Statistics ======================================================="
+
+for i in {0..4}; do
+for j in {0..4}; do
+  echo "!NetCDF(i,j)-> ${arr[$i]}-${arr[$j]}
+subroutine zonalstats_${arrid[$i]}${arrid[$j]}(map, mask, classfile)
+  character(*), optional, intent(in) :: classfile
+  type(nc2d_${arr[$j]}), optional, intent(in) :: mask
+  type(nc2d_${arr[$i]}), intent(in) :: map
+  type(zonal_${arr[$i]}) :: zstats
+  real(kind=double) :: sumquad
+  integer(kind=8) :: nlines, i, j, k
+
+!:Check if classfile is present ------------------------------------------
+  if(present(classfile))then
+    !Open file
+    open(100, file = classfile, status = 'old')
+
+    nlines = 0
+    do
+      read(100, *, end=101)
+      nlines = nlines +1
+    end do
+101 continue
+    rewind(100)
+
+    allocate(zstats%zclass(nlines))
+    allocate(zstats%zcount(nlines))
+    allocate(zstats%zsum(nlines))
+    allocate(zstats%zaverage(nlines))
+    allocate(zstats%zmin(nlines))
+    allocate(zstats%zmax(nlines))
+    allocate(zstats%zstdeviation(nlines))
+    allocate(zstats%zvariance(nlines))
+
+    do i = 1, nlines
+      read(100,*) zstats%zclass(i)
+    end do
+  !:Case classmap not present --------------------------------------------
+  else
+    nlines = 1
+    allocate(zstats%zclass(nlines))
+    allocate(zstats%zcount(nlines))
+    allocate(zstats%zsum(nlines))
+    allocate(zstats%zaverage(nlines))
+    allocate(zstats%zmin(nlines))
+    allocate(zstats%zmax(nlines))
+    allocate(zstats%zstdeviation(nlines))
+    allocate(zstats%zvariance(nlines))
+  end if
+
+!:Check if mask is present -----------------------------------------------
+  if(present(mask))then
+    do k = 1, nlines
+      zstats%zcount(k) = 0
+      zstats%zsum(k) = 0
+      zstats%zaverage(k) = 0
+      zstats%zmin(k) = 0
+      zstats%zmax(k) = 0
+      zstats%zstdeviation(k) = 0
+      zstats%zvariance(k) = 0
+      sumquad = 0
+
+      do i = 1, mask%nlons
+        do j = 1, mask%nlats
+          if(zstats%zclass(k).eq.mask%ncdata(j,i).and.map%ncdata(j,i).ne.map%f_value)then
+            zstats%zcount(k) = zstats%zcount(k) + 1
+            zstats%zsum(k) = zstats%zsum(k) + map%ncdata(j,i)
+            if(map%ncdata(j,i).ne.map%f_value)then
+              zstats%zmin(k) = min(map%ncdata(j,i), zstats%zmin(nlines))
+              zstats%zmax(k) = max(map%ncdata(j,i), zstats%zmax(nlines))
+              sumquad = sumquad + map%ncdata(j,i)*map%ncdata(j,i)
+            end if
+          end if
+        end do
+      end do
+      if(zstats%zcount(k).ne.0)then
+        zstats%zstdeviation(k) = sqrt((sumquad-(zstats%zsum(k)*zstats%zsum(k))/&
+                                       zstats%zcount(k))/(zstats%zcount(k)-1))
+        zstats%zvariance(k) = zstats%zstdeviation(k)*zstats%zstdeviation(k)
+        zstats%zaverage(k) = zstats%zsum(k)/zstats%zcount(k)
+      end if
+    end do
+  !:Case mask not present ------------------------------------------------
+  else
+    zstats%zcount(nlines) = 0
+    zstats%zsum(nlines) = 0
+    zstats%zaverage(nlines) = 0
+    zstats%zmin(nlines) = 0
+    zstats%zmax(nlines) = 0
+    zstats%zstdeviation(nlines) = 0
+    zstats%zvariance(nlines) = 0
+    sumquad = 0
+    do i = 1, map%nlons
+      do j = 1, map%nlats
+        if(map%ncdata(j,i).ne.map%f_value)then
+          zstats%zcount(nlines) = zstats%zcount(nlines) + 1
+          zstats%zsum(nlines) = zstats%zsum(nlines) + map%ncdata(j,i)
+          sumquad = sumquad + map%ncdata(j,i)*map%ncdata(j,i)
+        end if
+      end do
+    end do
+    zstats%zmin(nlines) = minval(map%ncdata, mask=map%ncdata.ne.map%f_value)
+    zstats%zmax(nlines) = maxval(map%ncdata, mask=map%ncdata.ne.map%f_value)
+    if(zstats%zcount(nlines).ne.0)then
+      zstats%zstdeviation(nlines) = sqrt((sumquad-(zstats%zsum(nlines)*zstats%zsum(nlines))/&
+                                          zstats%zcount(nlines))/(zstats%zcount(nlines)-1))
+      zstats%zvariance(nlines) = zstats%zstdeviation(nlines)*zstats%zstdeviation(nlines)
+      zstats%zaverage(nlines) = zstats%zsum(nlines)/zstats%zcount(nlines)
+    end if
+  end if
+
+  write(*,'(a8,a15,5a20)')"'"Class"'", "'"Count"'", "'"Sum"'", "'"Average"'", "'"Min"'", "'"Max"'", "'"Standard Deviation"'"
+  do i = 1, nlines
+    write(*,'(i8,i15,5f20.5)')zstats%zclass(i), zstats%zcount(i), zstats%zsum(i), &
+         zstats%zaverage(i), zstats%zmin(i), zstats%zmax(i), zstats%zstdeviation(i)
+  end do
+end subroutine zonalstats_${arrid[$i]}${arrid[$j]}
+"
+done
+done
+
