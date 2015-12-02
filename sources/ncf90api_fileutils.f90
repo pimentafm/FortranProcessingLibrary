11c149286472f59acfb18d16f3ac485edba60c60
Fernando (at work) <fernando.m.pimenta@gmail.com>
2015-12-02 16:07:45 -0200

    Added ncf90api_fileutils.f90

---
+++ sources/ncf90api_fileutils.f90
@@ -0,0 +1,42 @@
+!:========================================================================
+! This file is part of f90NetCDF API (Fortran 90 API for NetCDF).
+
+! Copyright (C) 2015 Fernando Martins Pimenta
+
+! f90NetCDF API is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+
+! f90NetCDF API is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+! You should have received a copy of the GNU General Public License
+! along with f90NetCDF.  If not, see <http://www.gnu.org/licenses/>.
+
+!:========================================================================
+!About Author:
+!Student of Surveying and Cartographic Engineering
+! Federal University of Viçosa - Brazil
+
+!Bachelor of Biosystems Engineer
+! Federal University of São João del-Rei - Brazil
+
+! Research Group on Atmosphere-Biosphere Interaction
+! Federal University of Viçosa
+! Data: August 09, 2015
+
+!Contacts: fernando.m.pimenta@gmail.com, fernando.m.pimenta@ufv.br
+!:========================================================================
+!File Manipulation subroutines ===========================================
+
+subroutine file_exists(ifile)
+  logical :: fexist
+  character(*) :: ifile
+
+  inquire(file=ifile, exist=fexist)
+  
+  if(fexist)write(*,*) ifile, " EXIST!"
+end subroutine file_exists
