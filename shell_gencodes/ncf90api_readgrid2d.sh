8c55d4de04c1fd8ee82031e292ef812ae3c0d7b3
Fernando (at work) <fernando.m.pimenta@gmail.com>
2016-01-06 14:57:27 -0200

    Parameters in check subroutine was changed

--- shell_gencodes/ncf90api_readgrid2d.sh
+++ shell_gencodes/ncf90api_readgrid2d.sh
@@ -57,11 +57,11 @@ subroutine readgrid2d_${arr[$i]}(ifile, idata)
   !Get Lons, Lats and variable values
   call check(nf90_inq_varid(ncid, idata%lonname, xvarid))
   call check(nf90_get_var(ncid, xvarid, idata%longitudes))
-  call checkatt(nf90_get_att(ncid, xvarid, "'"units"'", idata%lonunits),"'"degrees_east"'")
+  call checkatt(nf90_get_att(ncid, xvarid, "'"units"'", idata%lonunits),"'"lonunits"'")
 
   call check(nf90_inq_varid(ncid, idata%latname, yvarid))
   call check(nf90_get_var(ncid, yvarid, idata%latitudes))
-  call checkatt(nf90_get_att(ncid, yvarid, "'"units"'", idata%latunits), "'"degrees_north"'")
+  call checkatt(nf90_get_att(ncid, yvarid, "'"units"'", idata%latunits), "'"latunits"'")
 
   !Get Variable name
   call check(nf90_inq_varid(ncid, idata%varname, varid))
@@ -70,7 +70,7 @@ subroutine readgrid2d_${arr[$i]}(ifile, idata)
   !Get some attributes
   call checkatt(nf90_get_att(ncid, varid, "'"long_name"'", idata%long_name), "'"long_name"'")
   call checkatt(nf90_get_att(ncid, varid, "'"_FillValue"'", idata%f_value), "'"_FillValue"'")
-  call checkatt(nf90_get_att(ncid, varid, "'"units"'", idata%varunits),"'"variable units"'")
+  call checkatt(nf90_get_att(ncid, varid, "'"units"'", idata%varunits),"'"varunits"'")
 
   call check(nf90_close(ncid))
 end subroutine readgrid2d_${arr[$i]}
