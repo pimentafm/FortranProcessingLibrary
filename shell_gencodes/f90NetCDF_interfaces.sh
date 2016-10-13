db51e603868400dfe867b3fbd92fc593c1d7ce25
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2016-10-13 15:17:57 -0300

    changes in the comments

--- shell_gencodes/f90NetCDF_interfaces.sh
+++ shell_gencodes/f90NetCDF_interfaces.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "!:=============================================================================
-! This file is part of f90NetCDF API (Fortran 90 API for NetCDF).
+! This file is part of f90NetCDF (Fortran 90 API for NetCDF).
 
 ! Copyright (C) 2015 Fernando Martins Pimenta
 
-! f90NetCDF API is free software: you can redistribute it and/or modify
+! f90NetCDF is free software: you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
 ! the Free Software Foundation, either version 3 of the License, or
 ! (at your option) any later version.
 
-! f90NetCDF API is distributed in the hope that it will be useful,
+! f90NetCDF is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
@@ -19,16 +19,17 @@ echo "!:========================================================================
 ! along with f90NetCDF.  If not, see <http://www.gnu.org/licenses/>.
 
 !:=============================================================================
-!About Author:
-!Student of Surveying and Cartographic Engineering
-! Federal University of Viçosa - Brazil
+! About Author:
+! Fernando Martins Pimenta
+!  Student of Surveying and Cartographic Engineering
+!  Federal University of Viçosa - Brazil
 
-!Bachelor of Biosystems Engineer
-! Federal University of São João del-Rei - Brazil
+!  Bachelor of Biosystems Engineer
+!  Federal University of São João del-Rei - Brazil
 
-! Research Group on Atmosphere-Biosphere Interaction
-! Federal University of Viçosa
-! Data: August 09, 2015
+!  Research Group on Atmosphere-Biosphere Interaction
+!  Federal University of Viçosa
+!  Data: August 09, 2015
 
 !Contacts: fernando.m.pimenta@gmail.com, fernando.m.pimenta@ufv.br
 !:============================================================================="
@@ -62,8 +63,8 @@ declare -a arr2=("integer(kind=C_SIGNED_CHAR)" "integer(kind=C_SHORT)" "integer(
 echo "
 interface check
   module procedure checkerror, checktype, checkatt
-end interface check
-"
+end interface check"
+
 #NCOORDS
 echo "
 interface ncoords
