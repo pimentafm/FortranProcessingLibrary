d8ad8230e6c7eca08c6ad2df50c0cd73ccf79b02
Fernando <fernando.m.pimenta@gmail.com>
2017-01-15 16:28:01 -0200

    Test OpenMP (personal computer)

--- examples/ex0_libtest.f90
+++ examples/ex0_libtest.f90
@@ -2,6 +2,19 @@ program libtest
   use fpl
   implicit none
 
+
   write(*,*) fpl_libversion()
+
+  !OpenMP -------------------------------------------------------------
+  call omp_set_num_threads(3)
+
+  write(*,*) "Num procs  : ", omp_get_num_procs()
+  write(*,*) "Max threads: ", omp_get_max_threads()
+  write(*,*) "Num threads (serial region): ", omp_get_num_threads()
+
+ !$omp parallel
+   write(*,*) "Num threads (parallel region): ", omp_get_num_threads()
+ !$omp end parallel
+ !---------------------------------------------------------------------
 
 end program libtest
