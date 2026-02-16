ec6c96db8b75721603fe1901059839e466d66ec9
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2026-02-15 21:59:47 -0300

    Fix onBrokerMarkdownLinks

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,7 +14,12 @@ const config: Config = {
   projectName: 'FortranProcessingLibrary',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   i18n: {
     defaultLocale: 'en',
